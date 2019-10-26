16c7e12d55f5e541f65bb536d44fc2880d487f8a
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-26 14:12:09 +0000

    began scrapping

--- lib/CLI_GFC.rb
+++ lib/CLI_GFC.rb
@@ -1,3 +1,15 @@
+require "nokogiri"
+require "open-uri"
+require "pry"
+
 def run
-  puts "hello world"
+  puts "       Welcome to the Bullseye Class Listing"
+  puts "Upcoming Classes in Glass fusing, Kilncasting, Coldworking"
+  puts " "
+  puts "----------------------------------------------------------"
+  puts " "
+
+  html = open("https://classes.bullseyeglass.com/classes-events.html")
+  doc = Nokogiri::HTML(html)
+  binding.pry
 end

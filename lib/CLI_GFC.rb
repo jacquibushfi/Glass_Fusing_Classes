20b232fd2ef2a8853ea5e08ff59d3e7baa10a9a1
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-28 16:13:12 +0000

    Split out just the class title from the full class title

--- lib/CLI_GFC.rb
+++ lib/CLI_GFC.rb
@@ -11,14 +11,14 @@ class CLI
   Scraper.scrape_klasses
 
 # loop that asks user what to do until experimenting
-print_klasses
+  print_klasses
   menu
   end
-
 
   def menu
     #print_klasses
   end
+
   def print_klasses
     Klass.all.each.with_index(1) do |klass, index|
     puts "#{index}. #{klass.title}"
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,11 @@ class Scraper
     html = open("https://classes.bullseyeglass.com/classes-events.html")
     doc = Nokogiri::HTML(html)
 
-          doc.css("div.category-products").css("li.item.last").each do |item|
-          klass = Klass.new
-          klass.title = item.css(".product-name a[title]").text.strip
-          klass.link  = item.css("a").attribute("href").value
-          end
+      doc.css("div.category-products").css("li.item.last").each do |item|
+        klass = Klass.new
+        klass.title  = item.css(".product-name a[title]").text.strip.split(",")[0]
+        klass.link  = item.css("a").attribute("href").value
+      end
   end
 
   def self.scrape_klass_details(klass)

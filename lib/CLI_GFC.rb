fd3ca46b96c85ecbd02050d98b7e0402ae01e0e0
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-29 21:06:30 +0000

    menu completion

--- lib/CLI_GFC.rb
+++ lib/CLI_GFC.rb
@@ -1,46 +1,61 @@
 
 
 class CLI
-  attr_reader :input
 
   def run
+    puts " "
     puts "          Welcome to the Bullseye Class Listing"
     puts " "
-    puts "Upcoming Classes in Glass fusing, Kilncasting, and Coldworking"
+    puts "Upcoming Classes in Glass Fusing, Kilncasting, and Coldworking"
     puts "--------------------------------------------------------------"
     puts " "
-  Scraper.scrape_klasses
-  menu
-  input = gets.strip.downcase
 
-   while input != 'exit'  do
-  #   elsif input.to_i.between?(1, Klass.count)
-       klass = Klass.all[input.to_i - 1]
-       Scraper.scrape_klass_details(klass) #if !klass.title
-       display_klass_details(klass)
-       puts " "
-       puts "Enter 'list' to see the list, a class number, or 'exit' to exit"
-       input = gets.strip.downcase
-   end
-    puts "Goodbye"
+    Scraper.scrape_klasses
+    menu
+    input = gets.strip.downcase
+
+    while input != 'exit'  do
+      case
+      when input.to_i.between?(1, Klass.all.count)
+          klass = Klass.all[input.to_i - 1]
+          Scraper.scrape_klass_details(klass) if !klass.description
+          display_klass_details(klass)
+          input = gets.strip.downcase
+        when input == "list"
+          menu
+          input = gets.strip.downcase
+        else
+          puts "Please select from the options below"
+          selector_text
+          input = gets.strip.downcase
+        end
+    end
+      puts " "
+      puts "Goodbye!"
   end
 
-    def display_klass_details(klass)
-      puts "------------------------------------------------------------------------------------------------------------------------"
-      puts " "
-      puts "#{klass.title}"
-      puts "#{klass.daytimes}      Price: #{klass.price}      #{klass.availability} "
-      puts " "
-      puts "#{klass.description}"
-      puts "#{klass.notes}"
-   end
+  def display_klass_details(klass)
+    puts " "
+    puts "#{klass.title}"
+    puts " "
+    puts "#{klass.daytimes}      Price: #{klass.price}      #{klass.availability} "
+    puts " "
+    puts "#{klass.description}"
+    puts " "
+    puts "Notes:  #{klass.notes}"
+    selector_text
+  end
 
   def menu
-      Klass.all.each.with_index(1) do |klass, index|
-        puts "#{index}. #{klass.title}"
-      end
-    puts " "
-    puts "Please Select the number of a class to see more information,  type 'list' to see the list again, or  'exit' to exit"
+    Klass.all.each.with_index(1) do |klass, index|
+      puts "#{index}. #{klass.title}"
+    end
+    selector_text
   end
 
+  def selector_text
+    puts " "
+    puts "Please enter a class number to see the class details (1 - #{Klass.all.count} ),  'list' to see the list again, or  'exit' to exit"
+    puts "-----------------------------------------------------------------------------------------------------------------"
+  end
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,25 +1,23 @@
-
-
 class Scraper
 
   def self.scrape_klasses
     html = open("https://classes.bullseyeglass.com/classes-events.html")
     doc = Nokogiri::HTML(html)
 
-      doc.css("div.category-products").css("li.item.last").each do |item|
-        split_title = []
-        klass = Klass.new
-        klass.title  = item.css(".product-name a[title]").text.strip
-        klass.link  = item.css("a").attribute("href").value
-        split_title = klass.title.split(",")
-          if split_title[1].include? "with"
-            klass.location = split_title[2].strip
-            klass.date = split_title[3].strip + ", " + split_title[4].strip
-          else
-            klass.location = split_title[1].strip
-            klass.date = split_title[2].strip + ", " + split_title[3].strip
-          end
-      end
+    doc.css("div.category-products").css("li.item.last").each do |item|
+      split_title = []
+      klass = Klass.new
+      klass.title  = item.css(".product-name a[title]").text.strip
+      klass.link  = item.css("a").attribute("href").value
+      split_title = klass.title.split(",")
+        if split_title[1].include? "with"
+          klass.location = split_title[2].strip
+          klass.date = split_title[3].strip + ", " + split_title[4].strip
+        else
+          klass.location = split_title[1].strip
+          klass.date = split_title[2].strip + ", " + split_title[3].strip
+        end
+    end
   end
 
   def self.scrape_klass_details(klass)
@@ -31,5 +29,5 @@ class Scraper
     klass.notes = doc.css("div.std p")[3].text
     klass.availability = doc.css("div.extra-info p").text.strip.gsub(/\s+/, " ")
     klass
-   end
+  end
 end
